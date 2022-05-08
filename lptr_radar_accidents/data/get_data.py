5ba5509656e81e34e17aa9c59902e192a062f0c4
Timothee Ribes <timothee.ribes@epita.fr>
2022-05-08 23:22:54 +0200

    feat(done): main graph and explanation

--- lptr_radar_accidents/data/get_data.py
+++ lptr_radar_accidents/data/get_data.py
@@ -20,7 +20,7 @@ accidents_ressource = [
     {"sep" : ',' ,"date" : 2006 , "url" : "https://static.data.gouv.fr/resources/base-de-donnees-accidents-corporels-de-la-circulation/20160909-180009/caracteristiques_2006.csv"},
     {"sep" : ',' ,"date" : 2007 , "url" : "https://static.data.gouv.fr/resources/base-de-donnees-accidents-corporels-de-la-circulation/20160909-180642/caracteristiques_2007.csv"},
     {"sep" : ',' ,"date" : 2008 , "url" : "https://static.data.gouv.fr/resources/base-de-donnees-accidents-corporels-de-la-circulation/20160909-180939/caracteristiques_2008.csv"},
-    {"sep" : 't' ,"date" : 2009 , "url" : "https://static.data.gouv.fr/resources/base-de-donnees-accidents-corporels-de-la-circulation/20160422-111851/caracteristiques_2009.csv"},
+    {"sep" : '\t' ,"date" : 2009 , "url" : "https://static.data.gouv.fr/resources/base-de-donnees-accidents-corporels-de-la-circulation/20160422-111851/caracteristiques_2009.csv"},
     {"sep" : ',' ,"date" : 2010 , "url" : "https://static.data.gouv.fr/resources/base-de-donnees-accidents-corporels-de-la-circulation-sur-6-annees/20150806-155035/caracteristiques_2010.csv"},
     {"sep" : ',' ,"date" : 2011 , "url" : "https://static.data.gouv.fr/resources/base-de-donnees-accidents-corporels-de-la-circulation-sur-6-annees/20150806-154723/caracteristiques_2011.csv"},
     {"sep" : ',' ,"date" : 2012 , "url" : "https://static.data.gouv.fr/resources/base-de-donnees-accidents-corporels-de-la-circulation-sur-6-annees/20150806-154431/caracteristiques_2012.csv"},
@@ -33,34 +33,71 @@ accidents_ressource = [
 ]
 url_radars = "https://static.data.gouv.fr/resources/radars-automatiques/20181025-141231/radars.csv"
 
-def prepare_data():
+def prepare_accidents():
     # Download
     accidents = {}
+    dtype = {'lat':str,'long':str,'an':int, 'dep':int}
     for ressource in accidents_ressource:
-        accidents[ressource['date']] = pd.read_csv(ressource['url'], encoding='latin',on_bad_lines='skip',sep=ressource['sep'])
+        accidents[ressource['date']] = pd.read_csv(ressource['url'], encoding='latin',
+            on_bad_lines='skip',sep=ressource['sep'], dtype=dtype)
     df = pd.concat(accidents)
 
     # Processing
     df.loc[df['an'] < 2000,'an'] += 2000
+
+    def convert_dep(x):
+        if x % 10 == 0:
+            x =  x / 10
+        return int(x)
+
+    df['dep'] = df['dep'].apply(convert_dep).astype(str)
+    df = df[(df['long']=='-') == False]
     def convert_deg(x):
         if not isinstance(x, str) or x == '-':
             return np.nan
         return float(x[:2]+'.'+ x[2:])
+
     df.loc[:,'long'] = df['long'].apply(convert_deg)
     df.loc[:,'lat'] = df['lat'].apply(convert_deg)
 
-    # Saving
-    df.to_pickle("lptr_accidents.pkl")
+    df = df[["an", "adr", "lat", "long","Num_Acc", "dep"]]
 
+    # Saving
+    return df
+
+def prepare_radars():
     # Download
     df2 = pd.read_csv(url_radars)
 
     # Processing
-    radars_year = df2[["date_installation", "emplacement", "latitude", "longitude", "id"]].copy()
-    radars_year["date_installation"] = pd.DatetimeIndex(radars_year['date_installation']).year
+    df2["an"] = pd.DatetimeIndex(df2['date_installation']).year
+    outlier = df2.departement.unique()[-1]
+    df2 = df2.loc[df2['departement'] != outlier]
 
-    # Saving
-    radars_year.to_pickle("lptr_radars.pkl")
+    radars_year = df2[["an", "emplacement", "latitude", "longitude", "id", "departement"]].copy()
+    radars_year = radars_year.rename(columns=
+        {"latitude":"lat", "longitude":"long","departement":"dep"}).copy()
+
+    return radars_year
+
+def prepare_data():
+    radar_year = prepare_radars()
+    radar_year.to_pickle("lptr_radars.pkl")
+
+    df = prepare_accidents()
+    df_small = df[df['lat'] > 30]
+    df_small = df_small.dropna().copy()
+    df_small.to_pickle("lptr_accidents.pkl")
+
+    df = df.groupby(["dep","an"]).count().drop(columns=["long","lat","adr"])
+    radar_year = radar_year.groupby(["dep","an"]).count().drop(columns=["long","lat","emplacement"])
+
+    merge = df.merge(radar_year, on=["dep","an"])
+    merge = merge.reset_index().rename(columns={"Num_Acc":"nb d'accidents","id":"nb de radars"})
+    merge['nb de radars'] = merge['nb de radars'].cumsum()
+    merge.to_pickle("lptr_radar_accidents_dep.pkl")
+
+
 
 if __name__ == '__main__':
     prepare_data()
--- lptr_radar_accidents/radar_accidents.py
+++ lptr_radar_accidents/radar_accidents.py
@@ -6,9 +6,22 @@ from dash import Dash, dcc, html, Input, Output
 class Radar_Accidents():
     def __init__(self, application=None):
         self.df_radars = pd.read_pickle("data/lptr_radars.pkl")
+        self.df_accidents = pd.read_pickle("data/lptr_accidents.pkl")
+        self.dep_counts = pd.read_pickle("data/lptr_radar_accidents_dep.pkl")
 
         self.main_layout = html.Div(children=[
             html.H2('Impact des radars sur les accidents de la route en France'),
+            dcc.Graph(id='sm-dep-counts'),
+            dcc.Markdown("""
+                Il n'y a manifestement pas d'impact significatif de la quantité de radar sur le nombre d'accidents
+            """),
+            html.Br(),
+            html.P("Choix de la période :"),
+            dcc.Slider(2005, 2018, 1,
+                    value=2005,
+                    id="range-slider",
+                    marks={x:str(x) for x in range(2005,2019)}
+            ),
             html.Div(children=[
                 html.Div([
                     html.H4('Localisation des radars'),
@@ -19,12 +32,6 @@ class Radar_Accidents():
                     dcc.Graph(id="scatter-mapbox-accidents"),
                 ])
             ], style={'display':'flex'}),
-            html.P("Choix de la période :"),
-            dcc.Slider(2003, 2018, 1,
-                    value=2003,
-                    id="range-slider",
-                    marks={x:str(x) for x in range(2003,2019)}
-            ),
             html.Div([
                 html.H4('Sources :'),
                 html.A('accidents', href="https://www.data.gouv.fr/fr/datasets/bases-de-donnees-annuelles-des-accidents-corporels-de-la-circulation-routiere-annees-de-2005-a-2020/",style={'margin':'2px'}),
@@ -46,21 +53,52 @@ class Radar_Accidents():
 
         self.app.callback(
             Output("scatter-mapbox-accidents", "figure"),
-            Input("range-slider", "value")) (self.update_bar_chart)
-    # fonction à appliquer lorsque le callback arrive.
-    def update_bar_chart(self, slider_range):
+            Input("range-slider", "value")) (self.update_loc_accidents)
+        self.app.callback(
+            Output("sm-dep-counts", "figure"),
+            Input("range-slider", "value")
+        ) (self.update_graph)
+
+    def update_loc_accidents(self, slider_range):
         # le masque pour afficher seulement les radars installés à l'année indiquée par le slider
         # (y compris les années d'avant)
-        mask = self.df_radars['date_installation'] <= slider_range
+        mask = self.df_accidents['an'] == slider_range
 
         # la fonction d'affichage du graphique avec ses paramètres.
-        fig = px.scatter_mapbox(self.df_radars[mask],
-                                lat="latitude", lon="longitude",
-                                hover_name="emplacement", color_discrete_sequence=["blue"],
+        fig = px.scatter_mapbox(self.df_accidents[mask],
+                                lat="lat", lon="long",
+                                hover_name='adr', color_discrete_sequence=["blue"],
                                 zoom=3.75, height=425, width=425, center={'lat':46,'lon':2})
 
         # fonction pour afficher la map.
         fig.update_layout(mapbox_style="open-street-map")
+        return fig
+
+    def update_bar_chart(self, slider_range):
+        # le masque pour afficher seulement les radars installés à l'année indiquée par le slider
+        # (y compris les années d'avant)
+        mask = self.df_radars['an'] <= slider_range
+
+        # la fonction d'affichage du graphique avec ses paramètres.
+        fig = px.scatter_mapbox(self.df_radars[mask],
+                                lat="lat", lon="long",
+                                hover_name='emplacement', color_discrete_sequence=["blue"],
+                                zoom=3.75, height=425, width=425, center={'lat':46,'lon':2})
+
+        # fonction pour afficher la map.
+        fig.update_layout(mapbox_style="open-street-map")
+        return fig
+
+    def update_graph(self, slider_range):
+        #dfg = self.df.loc[year]
+        #dfg = dfg[dfg['region'].isin(regions)]
+        mask = self.dep_counts['an'] <= slider_range
+        fig = px.scatter(self.dep_counts[mask], x = "nb de radars", y = "nb d'accidents", 
+                         #title = f"{year}", cliponaxis=False,
+                         #size = "population", size_max=60, 
+                         #color = "region", color_discrete_map = self.continent_colors,
+                         hover_name="dep"
+        )
         return fig
 
 
